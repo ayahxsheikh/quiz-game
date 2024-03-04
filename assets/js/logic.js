a561ca75a683ecc57637a7251965543142d02637
A Sheikh <ayah@Ayah-MacBook-Air.local>
2024-03-04 23:39:13 +0000

    initial files

---
+++ assets/js/logic.js
@@ -0,0 +1,17 @@
+// Create Variables for DOM Elements
+
+// Add event listner to show questions
+
+// Create function to loop over questions array and display each question object
+// Loop over each question object and display array of choices 
+// Create new element button for each choice 
+// Append new html to questions div
+
+
+// Create function for when a button from choices is clicked; next questions appear
+
+// Add an if statement that decreases the timer by 10 secs on incorrect answers
+
+// End quiz when all questions are answered or when timer reaches 0
+
+// Store user-name and user-score to localStorage and display it on screen
---
+++ assets/js/questions.js
@@ -0,0 +1,22 @@
+let questions = [
+    {
+        question: "Which of the following countries are in South Asia",
+        choices: ['Mongolia', 'Thailand', 'Pakistan', 'Chechenya' ]
+    },
+    {
+        question: "What city is the capital of Turkey",
+        choices: ['Antalya', 'Isfahan', 'Istanbul', 'Busra']
+    },
+    {
+        question: "Which country is known for having a 'Seista'",
+        choices: ['Poland', 'Spain', 'Germany', 'Portugal']
+    },
+    {
+        question: "Which country is not in East Africa",
+        choices: ['Somalia', 'Tanzania', 'Nigeria', 'Ethopia']
+    },
+    {
+        question: "Which country has the colour green in their national flag",
+        choices: ['Argentina', 'Mexico', 'Coloumbia', 'Venezula']
+    }
+]
