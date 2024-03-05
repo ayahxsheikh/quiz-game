9a41cd424a9394b3a60a26f3b646d4a2e41903b8
A Sheikh <ayah@Ayah-MacBook-Air.local>
2024-03-05 00:17:13 +0000

    creates dom elements, startfucntion & timerfunc

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,6 +1,36 @@
 // Create Variables for DOM Elements
+let timer = document.querySelector('#timer');
+let startSc = document.querySelector('#start-screen');
+let startBtn = document.querySelector('#start');
+let endSc = document.querySelector('#end-screen');
+
 
 // Add event listner to show questions
+startBtn.addEventListener('click', function (event){
+    event.preventDefault();
+    startSc.setAttribute('class', 'hide');
+    countDown();
+    showQuestions();
+})
+
+// Create timer function
+function countDown(){
+    let timeLeft = 60;
+
+// create setInterval Function inside countdown to decrease timeLeft
+    let timeInterval = setInterval(function (){
+        timer.textContent = timeLeft;
+        timeLeft--;
+
+        if(timeLeft <= 0){
+            clearTimeout(timeInterval)
+            endSc.removeAttribute('class');
+        // TODO: incorportate end time and display end-screen when all questions are ansered 
+        }
+
+    }, 1000);
+
+}
 
 // Create function to loop over questions array and display each question object
 // Loop over each question object and display array of choices 
